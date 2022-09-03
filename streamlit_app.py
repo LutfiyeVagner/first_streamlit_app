8044f3dd17e49c21590fe438399feee3c7aaa3c2
Lütfiye Vagner <112546204+LutfiyeVagner@users.noreply.github.com>
2022-09-03 20:48:21 +0200

    Update streamlit_app.py
    identify the variable fruit_choice before using it

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 
 import requests
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 
@@ -37,5 +39,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # creates a data table
 streamlit.dataframe(fruityvice_normalized)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+
