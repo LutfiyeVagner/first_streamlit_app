db4add5d0c6395b5f70da6fe912e78a226bb89eb
Lütfiye Vagner <112546204+LutfiyeVagner@users.noreply.github.com>
2022-09-03 20:44:19 +0200

    Update streamlit_app.py
    deleting the line: streamlit.text(fruityvice_response.json()) #just writes data to screen
    
    adding kiwi to the table

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,8 +29,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) #just writes data to screen
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + kiwi")
+
 
 # normilise the data from json response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
